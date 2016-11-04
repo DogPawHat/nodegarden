0fe5a8006004e295fc08ecb7e4318267861e3c53
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2016-11-04 16:37:31 +0000

    Add basic types

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export = nodegarden;
+declare class nodegarden {
+    constructor(container: any);
+    render(start: any): void;
+    resize(): void;
+    start(): void;
+    stop(): void;
+    toggleNightMode(): void;
+}
